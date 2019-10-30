c1b120bf09cac5f5b44bcaf984b35d2021c72bd5
MissingPersonFGC <iam@coreylanier.com>
2019-10-29 23:36:32 -0400

    creating basic header

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar
+    color="primary"
+    dark
+    app
+  >
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title>Esports Experiences</v-toolbar-title>
+
+  </v-app-bar>
+</template>
