a5e419d52eb208a7e775cf5ca966a9bf717da761
MissingPersonFGC <iam@coreylanier.com>
2019-10-30 02:05:45 -0400

    created signup form

---
+++ src/components/Signup.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container>
+    <h2 class="display-1">Signup</h2>
+    <v-form>
+      <v-text-field
+        label="Email Address"
+        v-model="email"
+        prepend-icon="mdi-email"
+      />
+      <v-text-field
+        label="Password"
+        :type="show ? 'text' : 'password'"
+        prepend-icon="mdi-key"
+        :append-icon="show ? 'mdi-eye-off' : 'mdi-eye'"
+        @click:append="show = !show"
+        v-model="password"
+      />
+      <v-text-field
+        label="Verify Password"
+        :type="show ? 'text' : 'password'"
+        prepend-icon="mdi-key"
+        :append-icon="show ? 'mdi-eye-off' : 'mdi-eye'"
+        @click:append="show = !show"
+        v-model="verifyPassword"
+      />
+      <v-text-field
+        label="Username"
+        v-model="username"
+        prepend-icon="mdi-account"
+      />
+      <v-text-field
+        label="Location"
+        v-model="location"
+        prepend-icon="mdi-earth"
+      />
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "signup",
+  data() {
+    return {
+      email: "",
+      password: "",
+      verifyPassword: "",
+      username: "",
+      location: "",
+      show: false,
+      success: false,
+      loading: false,
+      error: null
+    };
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../components/Home.vue";
 import Login from "../components/Login.vue";
+import Signup from "../components/Signup.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: "/login",
     name: "login",
     component: Login
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: Signup
   }
 ];
 
