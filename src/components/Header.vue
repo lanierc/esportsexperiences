b4f0bd615e91eec5f0f22d870e0445de1c8c8413
MissingPersonFGC <iam@coreylanier.com>
2019-10-29 23:56:03 -0400

    finished basic header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,8 +4,14 @@
     dark
     app
   >
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon />
     <v-toolbar-title>Esports Experiences</v-toolbar-title>
-
+    <v-spacer />
+    <v-btn text>
+      <v-icon>mdi-login</v-icon> Login
+    </v-btn>
+    <v-btn text>
+      <v-icon>mdi-account-plus</v-icon> Signup
+    </v-btn>
   </v-app-bar>
 </template>
