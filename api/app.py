e7bfa1079f922206d38588e1761e532ca8e76b77
Corey Lanier <iam@coreylanier.com>
2020-05-11 15:47:52 -0400

    creating comments for code

--- api/app.py
+++ api/app.py
@@ -5,20 +5,24 @@ from mongoengine import *
 import os
 from datetime import datetime
 
+# Loading environmentals
 load_dotenv()
 
 
 DEBUG = True
 
+# Creating flask instance for routing
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-
+# setting up cors
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+# Connecting to MongoDB Atlas
 connect(os.getenv('MONGODB_URI'))
 
 
+# Creating User Model
 class User(Document):
     join_date: DateTimeField(required=True, default=datetime.now())
     username: StringField(required=True)
@@ -28,10 +32,12 @@ class User(Document):
     role: StringField(required=True, default="User")
 
 
+# Creating routes
 @app.route('/', methods=['GET'])
 def hello_world():
     return jsonify('Hello World')
 
 
+# start the server
 if __name__ == '__main__':
     app.run()
