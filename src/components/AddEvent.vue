a44f3aeb5e71f3365945e46fee2f105d2ca3ab77
Corey Lanier <iam@coreylanier.com>
2020-06-13 19:41:08 -0400

    created form for submitting events

--- src/components/AddEvent.vue
+++ src/components/AddEvent.vue
@@ -2,8 +2,52 @@
   <v-container>
     <div class="add-event">
       <h1>Add Event</h1>
-      <v-form>
-
+      <v-form @submit.prevent="submitEvent">
+        <v-text-field
+          v-model="name"
+          label="Event Name"
+          prepend-icon="mdi-calendar-blank-outline"
+        />
+        <v-text-field
+          v-model="location"
+          label="Event Location"
+          prepend-icon="mdi-earth"
+        />
+        <v-textarea
+          label="Description"
+          v-model="description"
+          prepend-icon="mdi-comment-text-outline"
+        />
+        <v-text-field
+          label="Website"
+          v-model="website"
+          prepend-icon="mdi-web"
+        />
+        <v-text-field
+          label="Facebook (Full URL)"
+          v-model="facebook"
+          prepend-icon="mdi-facebook"
+        />
+        <v-text-field
+          label="Twitter (Username Only)"
+          v-model="twitter"
+          prepend-icon="mdi-twitter"
+        />
+        <v-text-field
+          label="Instagram (Username Only)"
+          v-model="instagram"
+          prepend-icon="mdi-instagram"
+        />
+        <v-select
+          :items="genres"
+          v-model="genre"
+          label="Genre"
+          prepend-icon="mdi-palette-swatch"
+        />
+        <v-btn
+          type="submit"
+          color="primary"
+        >Submit Event</v-btn>
       </v-form>
     </div>
   </v-container>
@@ -23,7 +67,15 @@ export default {
       twitter: "",
       instagram: "",
       genre: "",
-      loading: false
+      loading: false,
+      genres: [
+        "Fighting Games",
+        "DOTA",
+        "League of Legends",
+        "Hearthstone",
+        "FPS",
+        "Various"
+      ]
     };
   },
   computed: {
