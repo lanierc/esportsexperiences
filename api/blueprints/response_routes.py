ea233f782040bd4a1620b0c9c983fb901e1207ec
Corey Lanier <iam@coreylanier.com>
2020-06-14 01:21:04 -0400

    created model for responses to reviews

--- api/blueprints/response_routes.py
+++ api/blueprints/response_routes.py
@@ -2,5 +2,11 @@ from flask import Blueprint, jsonify, request
 from blueprints.user_routes import User
 from blueprints.event_routes import Event
 import mongoengine as me
+from datetime import datetime
 
 response_routes = Blueprint('response_routes', __name__)
+
+class Response(me.Document):
+    user = me.ReferenceField(User, required=True)
+    response_data = me.DateTimeField(required=True, default=datetime.now())
+    body = me.StringField(required=True)
