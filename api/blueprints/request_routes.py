34c0416eb7309c9510613568808c98957599df01
Corey Lanier <iam@coreylanier.com>
2020-06-13 23:28:00 -0400

    createed model for user-created requests

---
+++ api/blueprints/request_routes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, jsonify, request
+from blueprints.user_routes import User
+from blueprints.event_routes import Event
+import mongoengine as me
+
+request_routes = Blueprint('request_routes', __name__)
+
+class Request(me.Document):
+    request_type = me.StringField(required=True)
+    user = me.ReferenceField(User, required=True)
+    event = me.ReferenceField(Event, required=True)
+    name = me.StringField(required=False)
+    location = me.StringField(required=False)
+    description = me.StringField(required=False)
+    website = me.StringField(required=False)
+    facebook = me.StringField(required=False)
+    twitter = me.StringField(required=False)
+    instagram = me.StringField(required=False)
+    genre = me.StringField(required=False)
+    proof = me.StringField(required=True)
