2f3f5f6cd34ed4f5353104baaeb250d3ae1adc9b
Corey Lanier <iam@coreylanier.com>
2020-06-14 01:11:52 -0400

    added ownership field to event document

--- api/blueprints/event_routes.py
+++ api/blueprints/event_routes.py
@@ -15,6 +15,7 @@ class Event(me.Document):
     instagram = me.StringField(required=False)
     genre = me.StringField(required=False)
     reviews = me.ListField(me.ReferenceField(Review))
+    owner = me.ReferenceField(me.ReferenceField(User))
 
 
 # create event
