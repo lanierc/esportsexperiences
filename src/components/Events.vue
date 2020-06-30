5d50e062962c7a52d94c8fb28155ca6d1c5517fc
Corey Lanier <iam@coreylanier.com>
2020-06-29 21:13:08 -0400

    created very basic event format

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -4,6 +4,16 @@
     <div v-for="(event, index) in events" :key="index">
       <h2>{{event.name}}</h2>
       <p class="subtext">{{event.location}}</p>
+      <p v-if="event.owner && event.owner !== ''"><span class="subheading">Owner:</span> {{event.owner}}</p>
+      <h3>About this event:</h3>
+      <p>{{event.description}}</p>
+      <p>
+        <span class="subtext">Social Links:</span> 
+	<v-btn text :href="event.website"><v-icon>mdi-web</v-icon></v-btn>
+	<v-btn text :href="event.facebook"><v-icon>mdi-facebook</v-icon></v-btn>
+	<v-btn text :href="`https://twitter.com/${event.twitter}`"><v-icon>mdi-twitter</v-icon></v-btn>
+	<v-btn text :href="`https://twitter.com/${event.instagram}`"><v-icon>mdi-instagram</v-icon></v-btn>
+      </p>
     </div>
   </v-container>
 </template>
